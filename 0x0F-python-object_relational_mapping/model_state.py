d25fac4577cfc66ae2f9db3d930ffaf1f3f56aaa
Enosuji <Enoosuji@gmail.com>
2024-01-19 17:07:45 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-"""contains the class definition of a State and an instance
-Base = declarative_base() """
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
 
+from sqlalchemy import (create_engine)
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
-
-
-class City(Base):
-    """ City Class inherit from Base declarative_base()
-    links to the MySQL table states
-    Attr:
-        id, name
-    """
-    __tablename__ = 'cities'
-    id = Column(Integer, unique=True, autoincrement=True, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('State.id'), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
